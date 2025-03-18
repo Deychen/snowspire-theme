36c22b98cba1e111303a6730ff1adb3d5702c2b2
Deychen <142891833+Deychen@users.noreply.github.com>
2025-03-18 19:57:18 +0100

    footer content done

--- footer.php
+++ footer.php
@@ -16,15 +16,30 @@
 	<div class="logo-container">
 		<img src="<?php echo esc_url(get_template_directory_uri() . './assets/images/Snowspire 3.png'); ?>" alt="Snowspire Logo">
 	</div>
+
 	<div class="footer-content">
-	<img src="<?php echo esc_url(get_template_directory_uri() . './assets/images/youtube.png'); ?>" alt="Snowspire Logo">
-	<img src="<?php echo esc_url(get_template_directory_uri() . './assets/images/facebook.png'); ?>" alt="Snowspire Logo">
-	<img src="<?php echo esc_url(get_template_directory_uri() . './assets/images/instagram.png'); ?>" alt="Snowspire Logo">
-	<img src="<?php echo esc_url(get_template_directory_uri() . './assets/images/linkedin.png'); ?>" alt="Snowspire Logo">
-	
+		<img id="footer-logo-1" src="<?php echo esc_url(get_template_directory_uri() . './assets/images/youtube.png'); ?>" alt="Snowspire Logo">
+		<img id="footer-logo-2" src="<?php echo esc_url(get_template_directory_uri() . './assets/images/facebook.png'); ?>" alt="Snowspire Logo">
+		<img id="footer-logo-3" src="<?php echo esc_url(get_template_directory_uri() . './assets/images/instagram.png'); ?>" alt="Snowspire Logo">
+		<img id="footer-logo-4" src="<?php echo esc_url(get_template_directory_uri() . './assets/images/linkedin.png'); ?>" alt="Snowspire Logo">
+
 	</div>
+	<div id="address-list">
+		<ul>
+			<li>Address:</li>
+			<li>Thissnowyroad 12A</li>
+			<li>15634 Iceicebaby</li>
+		</ul>
+	</div>
+	<div id="contact-list">
+		<ul>
+			<li>Email Us:</li>
+			<li>snowspire@email.com</li>
+			<li>Call Us:</li>
+			<li>123 4567890</li>
+		</ul>
 </footer>
-</div><!-- #page -->
+</div>
 
 <?php wp_footer(); ?>
 
